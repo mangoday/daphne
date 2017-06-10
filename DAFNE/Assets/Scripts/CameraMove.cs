4bdd5834264f7e482a12c17e60ac112c357d1585
Woo Bin Jeong <eunwoo0312@naver.com>
2017-06-10 22:07:00 +0900

    first

---
+++ DAFNE/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 이 스크립트는 메인 카메라의 이동을 담당합니다.
+// 메인카메라는 우선은 앞으로 계속 움직입니다.
+//  - 필요한 것 -
+//  - moveSpeed
+//  - moveDirection
+public class CameraMove : MonoBehaviour {
+
+    Vector3 direction;
+    public float moveSpeed;
+    public GameObject Target;
+	// Use this for initialization
+	void Start () {
+        moveSpeed = .1f;
+        direction =  Target.transform.up - transform.forward;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(direction * moveSpeed * Time.deltaTime);
+	}
+}
---
+++ DAFNE/Assets/Scripts/DestroyZone.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 이 스크립트는 다프네를 뒤에서 따라가는 아폴론의 태양의 역할을 합니다.
+// 이 오브젝트를 지나쳐가는 물체들을 없애줍니다.
+public class DestroyZone : MonoBehaviour {
+
+    // 만약 지나쳐가는 물체가 길, 장애물 이라면 파괴.
+    // 다프네라면 엔딩(나무로 변화)
+    private void OnTriggerExit(Collider other)
+    {
+        //if(other.tag.Contains("벽"))
+        //{
+        //    Destroy(other.gameObject);
+        //}
+        Destroy(other.gameObject);
+    }
+}
