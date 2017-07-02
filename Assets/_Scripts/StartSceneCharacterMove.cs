959dc1e1d5d455660345679d74fbac6ab3fb53ac
snowwalk <snowwalk0205@gmail.com>
2017-07-02 21:38:23 +0900

    feat: startScene

---
+++ Assets/_Scripts/StartSceneCharacterMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartSceneCharacterMove : MonoBehaviour {
+
+public float scrollSpeed = 0.5F;
+    public Renderer rend;
+
+
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<Renderer>();
+		Vector3 destination = transform.position +8*Vector3.down;
+		
+		iTween.MoveTo(gameObject, iTween.Hash("position", destination, "time", 5f) );
+		StartCoroutine(OffsetMove());
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator OffsetMove()
+	{
+		float offset = Time.time * scrollSpeed;
+        rend.material.mainTextureOffset = new Vector2(offset, 0);
+		yield return null;
+	}
+}
